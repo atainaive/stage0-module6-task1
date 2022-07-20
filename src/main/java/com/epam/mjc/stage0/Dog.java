0dec3b840bb9e0074cae4d851b61493cde8da7be
atainaive <atai.omorov@stud.vgtu.lt>
2022-07-20 14:29:12 +0300

    committed

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,7 +1,12 @@
 package com.epam.mjc.stage0;
 
 public class Dog extends Animal {
-    public Dog(String color, int numberOfPaws, boolean hasFur) {
-        super(color, numberOfPaws, hasFur);
+    public Dog() {
+        super("brown", 4, true);
+    }
+
+    @Override
+    public String getDescription(){
+        return super.getDescription();
     }
 }
